b94e4182cd96af8e7b0d9a9c2631c23e576eb08a
ashwin <ashwinmangalore05@gmail.com>
2025-03-06 19:58:13 +0530

    modified the factorial function

--- src/pkg1/Fact1.java
+++ src/pkg1/Fact1.java
@@ -1,15 +1,20 @@
+
 package pkg1;
 
 public class Fact1 {
-
-	public static void main(String[] args) {
-		int num1=5;
+	public static int findFactorial(int num){
 		int fact1=1;
 		
-		for(int i=1;i<num1+1;i++) {
+		for(int i=1;i<num+1;i++) {
 			fact1=fact1*i;
 		}
-		System.out.println(fact1);
+		return fact1;
+	}
+
+	public static void main(String[] args) {
+		int num1 = 5;
+		int factorial=findFactorial(num1);
+		System.out.println("Factorial of "+num1+" is "+factorial);
 
 	}
 
