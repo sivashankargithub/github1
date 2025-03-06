25f12e4a754fa0dce3be2ef7f2c2555bb17b631f
Sivashankar C <sivashankarchandrappa.sangamone@gmail.com>
2025-03-06 19:39:34 +0530

    Factorial of 5

---
+++ src/pkg1/Fact1.java
@@ -0,0 +1,16 @@
+package pkg1;
+
+public class Fact1 {
+
+	public static void main(String[] args) {
+		int num1=5;
+		int fact1=1;
+		
+		for(int i=1;i<num1+1;i++) {
+			fact1=fact1*i;
+		}
+		System.out.println(fact1);
+
+	}
+
+}
